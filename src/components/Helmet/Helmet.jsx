bbe4fdc7d860599a25f84409a403b4fe7d1fc560
ENIOLA-THE-DATA-ANALYST <enioladebanjo911@gmail.com>
2023-08-03 02:22:06 +0100

    logo

--- src/components/Helmet/Helmet.jsx
+++ src/components/Helmet/Helmet.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Helmet = (props) => {
-    
     document.title = 'Renllugs Building Resources - ' + props.title
   return (
     <div className='w-100'>{props.children}</div>
